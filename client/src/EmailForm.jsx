d827b29b3beedad8a357350ab9b35ac070b29dcf
kavyansh18 <kavyanshkumar18@gmail.com>
2024-07-04 01:28:44 +0530

    feat/font-color

--- client/src/EmailForm.jsx
+++ client/src/EmailForm.jsx
@@ -377,7 +377,7 @@ export default function EmailForm() {
             />
             <br className="my-4" />
             <button
-              className="inline-flex h-10 mt-3 animate-shimmer items-center justify-center rounded-md border border-blue-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 lg:ml-[200px] ml-[120px]"
+              className="inline-flex h-10 mt-3 animate-shimmer items-center justify-center rounded-md border border-blue-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 lg:ml-[200px] ml-[110px] mb-8"
               type="submit"
             >
               {sendButtonText}
