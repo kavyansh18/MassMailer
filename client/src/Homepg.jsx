08d0911ec94964f77a0f1e3aa5d577da4670eade
kavyansh18 <kavyanshkumar18@gmail.com>
2024-07-04 10:46:20 +0530

    feat/socials

--- client/src/Homepg.jsx
+++ client/src/Homepg.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Meteors from "./UI/Meteors.jsx";
 import logo from "../src/assets/logo.png";
+import bmc from "../src/assets/bmc.png";
+import ig from "../src/assets/ig.svg";
+import github from "../src/assets/github.svg";
+import twitter from "../src/assets/twitter.svg";
 import { NavLink } from "react-router-dom";
 
 export function Homepg() {
@@ -42,6 +46,35 @@ export function Homepg() {
           {/* Meaty part - Meteor effect */}
           <Meteors number={20} />
         </div>
+      </div>
+      <div className="flex justify-center items-center gap-4">
+        <a
+          className="lg:w-12 lg:mt-12 w-12 mt-10"
+          href="https://instagram.com/kavyansh.in"
+        >
+          <img src={ig} alt="" />
+        </a>
+
+        <a
+          className="lg:w-24 lg:mt-12 w-20 mt-10"
+          href="https://buymeacoffee.com/kavyansh"
+        >
+          <img src={bmc} alt="" />
+        </a>
+
+        <a
+          className="lg:w-7 lg:mt-12 w-7 mt-10"
+          href="https://github.com/kavyansh18"
+        >
+          <img src={github} alt="" />
+        </a>
+
+        <a
+          className="lg:w-7 lg:mt-12 w-7 mt-10"
+          href="https://x.com/the_kavyansh"
+        >
+          <img src={twitter} alt="" />
+        </a>
       </div>
     </div>
   );
